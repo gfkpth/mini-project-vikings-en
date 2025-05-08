4234b264267c88a060b18b2c2e45ecbbc3c557a5
gfkpth <groeg3@web.de>
2025-05-08 18:24:33 +0200

    Completed classes; implemented main game logic, extended Viking attacks to introduce an effect of battle cry
    Co-authored-by: Ekaterina Kuznetsova <EkaterinaKuznetsova89@users.noreply.github.com>

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -19,8 +19,6 @@ class Soldier:
             self.health = 0
 
 
-        
-    
 
 # Viking
 
@@ -39,44 +37,81 @@ class Viking(Soldier):
         else:
             self.health = 0
             return f'{self.name} has died in act of combat'
+        # self.health -= damage
+        # if self.health > 0:
+        #     return f'{self.name} has received {damage} points of damage'
+        # else:
+        #     return f'{self.name} has died in act of combat'
 
 # Saxon
 
 class Saxon(Soldier):
-    def __init__(self, health, strength):
-        # your code here
-        pass
-        
 
     def receiveDamage(self, damage):
-        # your code here
-        pass
+        if self.health > damage:
+            self.health -= damage
+            return f'A Saxon has received {damage} points of damage'
+        else: 
+            self.health = 0
+            return 'A Saxon has died in combat'
+        # self.health -= damage
+        # if self.health > 0:
+        #     return f'A Saxon has received {damage} points of damage'
+        # else:
+        #     return 'A Saxon has died in combat'
 
 # Davicente
 
 class War():
     def __init__(self):
-        # your code here
-        pass
+        self.vikingArmy = []
+        self.saxonArmy = []
 
     def addViking(self, viking):
-        # your code here
-        pass
+        self.vikingArmy.append(viking)
     
     def addSaxon(self, saxon):
-        # your code here
-        pass
+        self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
-        # your code here
-        pass
+        attacker = random.choice(self.vikingArmy)
+        target = random.choice(self.saxonArmy)
+        
+        # there's a 15% chance for a Viking to use a battle cry
+        # the battle cry increases their attack by 3
+        if random.randrange(100) < 15:
+            print(attacker.name,': ', attacker.battleCry())     # shouting the battle cry
+            result = target.receiveDamage(attacker.attack()+3)  # increased attack strength
+        else:
+            result = target.receiveDamage(attacker.attack())    # regular attack
+        
+        
+        if target.health == 0:
+            self.saxonArmy.remove(target)
+        
+        return result
     
     def saxonAttack(self):
-        # your code here
-        pass
+        attacker = random.choice(self.saxonArmy)
+        target = random.choice(self.vikingArmy)
+        
+        result = target.receiveDamage(attacker.attack())
+        
+        if target.health == 0:
+            self.vikingArmy.remove(target)
+        
+        return result
+        
 
     def showStatus(self):
-        # your code here
-        pass
+        if self.saxonArmy == []:
+            return "Vikings have won the war of the century!"
+        elif self.vikingArmy == []:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
+        
+            
+
 
 
